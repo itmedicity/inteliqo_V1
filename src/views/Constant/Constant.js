22eed8f0f1fd5126b15cec42d23e33a9097c3c45
unknown <Administrator@MEDICITY.CO.IN>
2022-07-11 10:44:46 +0530

    new changes

--- src/views/Constant/Constant.js
+++ src/views/Constant/Constant.js
@@ -1,6 +1,6 @@
 import { axioslogin } from "../Axios/Axios";
 
-export const API_URL = 'http://192.168.10.170:5000/api';
+export const API_URL = 'http://192.168.10.206:5000/api';
 // export const API_URL = 'http://localhost:5000/api';
 
 export const employeeNumber = () => {
