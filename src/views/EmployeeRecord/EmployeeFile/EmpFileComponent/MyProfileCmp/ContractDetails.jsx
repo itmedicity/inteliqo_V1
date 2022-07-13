526d14976c5e30652b35b7d11e34535598d1fa42
itmedicity <93715223+itmedicity@users.noreply.github.com>
2022-07-13 11:52:41 +0530

    Update ContractDetails.jsx

--- src/views/EmployeeRecord/EmployeeFile/EmpFileComponent/MyProfileCmp/ContractDetails.jsx
+++ src/views/EmployeeRecord/EmployeeFile/EmpFileComponent/MyProfileCmp/ContractDetails.jsx
@@ -1,5 +1,5 @@
 import { ListItem, ListItemText, Typography } from '@mui/material';
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState,memo } from 'react'
 
 
 const ContractDetails = ({ ContractDetail }) => {
@@ -49,4 +49,4 @@ const ContractDetails = ({ ContractDetail }) => {
     )
 }
 
-export default ContractDetails
+export default memo(ContractDetails)
