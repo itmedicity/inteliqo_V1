5a34df060d0a2962b0b66d806cad9f314654f702
Ajith Arjun <ajithajeesh@gmail.com>
2022-07-15 18:29:51 +0530

    'githeck'

--- src/views/Constant/ApiLink.jsx
+++ src/views/Constant/ApiLink.jsx
@@ -1,4 +1,2 @@
 export const API_URL = 'http://192.168.10.170:5000/api';
-export const PUBLIC_NAS_FOLDER = "http://192.168.10.170/NAS/"
-console.log(API_URL)
-console.log(API_URL)
+export const PUBLIC_NAS_FOLDER = "http://192.168.10.170/NAS/";
