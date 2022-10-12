6d2c7c6fc425c94327000c78876d4ba432608bc6
unknown <Administrator@ITSYS13.medicity.co.in>
2022-10-12 10:25:59 +0530

    performance appraisal self approval

--- src/views/PerformanceAppraisal/SelfApproval/SelfApproval.jsx
+++ src/views/PerformanceAppraisal/SelfApproval/SelfApproval.jsx
@@ -1,4 +1,4 @@
-import { Box, Paper } from '@mui/material'
+import { Box, Grid, Paper } from '@mui/material'
 import React, { Fragment } from 'react'
 import { useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
@@ -11,6 +11,7 @@ const SelfApproval = () => {
         return state.getProfileData.ProfileData[0]
     })
     console.log(login);
+    const { em_no, em_name, sect_name, desg_name } = login
 
 
     const history = useHistory()
@@ -21,18 +22,56 @@ const SelfApproval = () => {
     return (
         <Fragment>
             <PageLayoutCloseOnly
-                heading="Performance Appraisal Approval HOD"
+                heading="Self Performance Appraisal Approval"
                 redirect={RedirectToProfilePage}
             >
-                <Paper square elevation={2} sx={{ p: 0.5, }}>
-                    <Box>
-                        <TextInput
-                            type="text"
-                            classname="form-control form-control-sm"
-                            Placeholder="Department"
-                            disabled="disabled"
-                        //value={dept_name}
-                        />
+                <Paper square elevation={2} >
+                    <Box sx={{
+                        //backgroundColor: "red",
+
+                    }}>
+                        <Grid container item xl={12} lg={12} spacing={1} direction="row"
+                            justifyContent="center"
+                            alignItems="center" sx={{
+                                p: 1
+                            }}>
+                            <Grid item xl={3} lg={3}>
+                                <TextInput
+                                    type="text"
+                                    classname="form-control form-control-sm"
+                                    Placeholder="Employee ID"
+                                    disabled="disabled"
+                                    value={em_no}
+                                />
+                            </Grid>
+                            <Grid item xl={3} lg={3}>
+                                <TextInput
+                                    type="text"
+                                    classname="form-control form-control-sm"
+                                    Placeholder="Emplyee Name"
+                                    disabled="disabled"
+                                    value={em_name}
+                                />
+                            </Grid>
+                            <Grid item xl={3} lg={3}>
+                                <TextInput
+                                    type="text"
+                                    classname="form-control form-control-sm"
+                                    Placeholder="Designation"
+                                    disabled="disabled"
+                                    value={desg_name}
+                                />
+                            </Grid>
+                            <Grid item xl={3} lg={3}>
+                                <TextInput
+                                    type="text"
+                                    classname="form-control form-control-sm"
+                                    Placeholder="Department Section"
+                                    disabled="disabled"
+                                    value={sect_name}
+                                />
+                            </Grid>
+                        </Grid>
                     </Box>
                 </Paper>
             </PageLayoutCloseOnly>
