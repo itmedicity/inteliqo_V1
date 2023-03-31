f73bf6df76e1e0c22acb946216bc3943efd32085
unknown <Administrator@ITSYS13.medicity.co.in>
2023-03-31 15:47:39 +0530

    leave approval redux, deleted old appraisal redux

--- src/views/LeaveManagement/ApprovalCEO/ApprovalCEO.jsx
+++ src/views/LeaveManagement/ApprovalCEO/ApprovalCEO.jsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router'
 import { ceoLeavereq, getleaverequest, CEohalfdayrequest, getCEOnopunchrequst, compensatoryCeo, getAllCeo, infoNofity } from 'src/views/CommonCode/Commonfunc';
 import PageLayoutCloseOnly from 'src/views/CommonCode/PageLayoutCloseOnly';
 import { Box, IconButton, Paper, Tooltip } from '@mui/material';
-import { Checkbox, CssVarsProvider, Typography } from '@mui/joy';
+import { CssVarsProvider } from '@mui/joy';
 import CommonAgGrid from 'src/views/Component/CommonAgGrid'
 import DeptSectionAllSelect from 'src/views/MuiComponents/DeptSectionAllSelect'
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
@@ -15,6 +15,7 @@ import HaldayRqModel from '../LeaveCommonComponent/HaldayRqModel'
 import NopunchRqModel from '../LeaveCommonComponent/NopunchRqModel'
 import CompOffRqModel from '../LeaveCommonComponent/CompOffRqModel'
 import _ from 'underscore';
+import MappingCheckbox from 'src/views/MuiComponents/MappingCheckbox';
 
 const ApprovalCEO = () => {
 
@@ -23,7 +24,7 @@ const ApprovalCEO = () => {
     const [allData, setalldata] = useState([])
     const [deptsect, setDeptSect] = useState(0)
 
-    const [levtpevalue, setleavetypevalue] = useState([])
+    const [levtpevalue, setleavetypevalue] = useState(1)
     const [leaverequesttype, setleaverequesttype] = useState([]);
     const [leavereq, setleavereqst] = useState([])
     const [deptlvRqst, setDptlvRqst] = useState([])
@@ -53,14 +54,7 @@ const ApprovalCEO = () => {
 
 
     const [reqtype, setreqtype] = useState([])
-    const [levtpevaluearry, setleavetypevaluearry] = useState({
-        COFF: false,
-        HDLR: false,
-        LR: false,
-        NOP: false,
-    }
-    )
-    const { COFF, HDLR, LR, NOP } = levtpevaluearry
+
 
     const [count, setcount] = useState(0)
     const [leaveremastdata, setleavereqmastdata] = useState([
@@ -114,52 +108,52 @@ const ApprovalCEO = () => {
 
     //use Effect for filtering leave request against  selected department section
     useEffect(() => {
-        if (deptsect !== 0 && levtpevalue === '1') {
+        if (deptsect !== 0 && levtpevalue === 1) {
             // depsection change filter based on dept section leave request
             const filterleavereq = leavereqmast.filter((val) => {
                 return (val.dept_section === deptsect)
             })
             setDptlvRqst(filterleavereq)
-        } else if (levtpevalue === '3' && deptsect !== 0) {
+        } else if (levtpevalue === 3 && deptsect !== 0) {
             // depsection change filter based on dept section no punch
             const filternopunch = nopunchmast.filter((val) => {
                 return (val.dept_section === deptsect)
             })
             setdeptNopunch(filternopunch)
-        } else if (levtpevalue === '2' && deptsect !== 0) {
+        } else if (levtpevalue === 2 && deptsect !== 0) {
             // depsection change filter based on dept section halfday
             const filterhalfday = halfdaymast.filter((val) => {
                 return (val.dept_section === deptsect)
             })
             setDepthlfday(filterhalfday)
 
-        } else if (levtpevalue === '4' && deptsect !== 0) {
+        } else if (levtpevalue === 4 && deptsect !== 0) {
             // depsection change filter based on dept section setcompensetory
             const filtercompen = compensetorymast.filter((val) => {
                 return (val.dept_section === deptsect)
             })
             setDeptCoff(filtercompen)
-        } else if (levtpevalue === '1' && deptsect === 0) {
+        } else if (levtpevalue === 1 && deptsect === 0) {
             if (Object.keys(leavereq).length > 0) {
                 setLeavRqstAll(leavereq)
             } else {
                 infoNofity("No Leave request pending!!")
             }
 
-        } else if (levtpevalue === '2' && deptsect === 0) {
+        } else if (levtpevalue === 2 && deptsect === 0) {
             if (Object.keys(halfday).length > 0) {
                 setHalfdayAll(halfday)
             } else {
                 infoNofity("No Leave request pending!!")
             }
-        } else if (levtpevalue === '3' && deptsect === 0) {
+        } else if (levtpevalue === 3 && deptsect === 0) {
             if (Object.keys(nopunch).length > 0) {
                 setnopunchall(nopunch)
             } else {
                 infoNofity("No Leave request pending!!")
             }
 
-        } else if (levtpevalue === '4' && deptsect === 0) {
+        } else if (levtpevalue === 4 && deptsect === 0) {
             if (Object.keys(compensetory).length > 0) {
                 setCoffAll(compensetory)
             } else {
@@ -171,19 +165,7 @@ const ApprovalCEO = () => {
     const RedirectToProfilePage = () => {
         history.push(`/Home`)
     }
-    const leverequesttypechange = async (e) => {
-        const ob1 = {
-            COFF: false,
-            HDLR: false,
-            LR: false,
-            NOP: false,
 
-        }
-        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-        setleavetypevaluearry({ ...ob1, [e.target.name]: value })
-        setleavetypevalue(e.target.value)
-
-    }
 
     const [columnDef] = useState([
         { headerName: 'Slno', field: 'SlNo', filter: true, minWidth: 100 },
@@ -297,20 +279,20 @@ const ApprovalCEO = () => {
                         <CssVarsProvider>
                             {
                                 leaverequesttype && leaverequesttype?.map((val, idx) => {
-                                    return <Checkbox
-                                        label={<Typography level="h2" fontSize="sm" sx={{ mb: 0 }} color="neutral" >{val.lrequest_type}</Typography>}
-                                        key={idx}
-                                        size="lg"
-                                        sx={{ flex: 1 }}
-                                        value={val.lrequest_slno}
-                                        name={val.lrequest_short}
-                                        checked={val.lrequest_short === 'LR' ? LR :
-                                            val.lrequest_short === 'HDLR' ? HDLR :
-                                                val.lrequest_short === 'NOP' ? NOP :
-                                                    val.lrequest_short === 'COFF' ? COFF : false
-                                        }
-                                        onChange={(e) => leverequesttypechange(e)}
-                                    />
+                                    return <Box sx={{
+                                        display: 'flex', p: 1,
+                                        width: { xl: "100%", lg: "100%", md: "100%", sm: "100%" }
+                                    }}
+                                        key={val.lrequest_slno}
+                                    >
+                                        <MappingCheckbox
+                                            label={val.lrequest_short}
+                                            name={val.lrequest_short}
+                                            value={val.lrequest_slno}
+                                            onChange={setleavetypevalue}
+                                            checkedValue={levtpevalue}
+                                        />
+                                    </Box>
                                 })
                             }
                         </CssVarsProvider>
@@ -338,86 +320,6 @@ const ApprovalCEO = () => {
                     />
                 </Paper>
             </PageLayoutCloseOnly>
-
-
-
-            {/* <PageLayoutSave
-                heading="Leave Approval CEO"
-                redirect={RedirectToProfilePage}
-            >
-                <div className="row g-1">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="col-md-12 col-sm-12 col-xs-12">
-                                <div className="row g-1">
-                                    <div className="col-md-6 col-sm-12 col-xs-12">
-                                        <div className="row g-1">
-                                            <div className="d-flex justify-content-around">
-                                                {
-                                                    leaverequesttype && leaverequesttype.map((val) => {
-                                                        return <div className="col-md-2" key={val.lrequest_slno} >
-                                                            <FormControlLabel
-                                                                control={
-                                                                    <Tooltip title={val.lrequest_type}>
-                                                                        <Checkbox
-                                                                            name={val.lrequest_short}
-                                                                            color="secondary"
-                                                                            value={val.lrequest_slno}
-                                                                            checked={val.lrequest_short === 'LR' ? LR :
-                                                                                val.lrequest_short === 'HDLR' ? HDLR :
-                                                                                    val.lrequest_short === 'NOP' ? NOP :
-                                                                                        val.lrequest_short === 'COFF' ? COFF : false
-
-                                                                            }
-                                                                            onChange={(e) => leverequesttypechange(e)}
-                                                                        />
-                                                                    </Tooltip>
-                                                                }
-                                                                label={val.lrequest_short}
-                                                            />
-                                                        </div>;
-                                                    })
-                                                }
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6 col-sm-12 col-xs-12">
-                                        <div className="row">
-                                            <div className="d-flex justify-content-start">
-                                                <div className="col-md-5 pt-1">
-                                                    <DeptSectionMastSelect
-                                                        style={SELECT_CMP_STYLE}
-                                                    />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="pt-2">
-                    <div className="card ">
-                        <div className="card-body">
-                            <div className="col-md-12">
-                                <ApprovalInchargeTable leavereq={
-                                    levtpevalue === '1' ? leavereq :
-                                        levtpevalue === '2' ? halfday :
-                                            levtpevalue === '4' ? compensetory :
-                                                levtpevalue === '3' ? nopunch : []
-                                } levtpevalue={levtpevalue} authority={3}
-                                    setleavereq={levtpevalue === '1' ? setleavereqst :
-                                        levtpevalue === '2' ? sethalfday :
-                                            levtpevalue === '4' ? setcompensetory :
-                                                levtpevalue === '3' ? setnopunch : null}
-
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </PageLayoutSave> */}
         </Fragment>
     )
 }
