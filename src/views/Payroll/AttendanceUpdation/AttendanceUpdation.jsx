5c447e197d4be35968ac2fd7679beb447be61aa6
unknown <Administrator@ITSYS13.medicity.co.in>
2023-04-27 12:29:01 +0530

    employee experience date field change, attendance updation change

--- src/views/Payroll/AttendanceUpdation/AttendanceUpdation.jsx
+++ src/views/Payroll/AttendanceUpdation/AttendanceUpdation.jsx
@@ -11,13 +11,13 @@ import { Actiontypes } from 'src/redux/constants/action.type'
 import _ from 'underscore'
 
 const AttendanceUpdation = () => {
- 
-  const [excel,setExcel]=useState([])
+
+  const [excel, setExcel] = useState([])
 
   const reduxDispatch = useDispatch()
-  const { GET_EXCEL_DATA} = Actiontypes;
+  const { GET_EXCEL_DATA } = Actiontypes;
 
-  const excelData = useSelector((state) =>  state.getExcelData.excelList, _.isEqual);
+  const excelData = useSelector((state) => state.getExcelData.excelList, _.isEqual);
 
   const excelDataList = useMemo(() => excelData, [excelData]);
 
@@ -29,39 +29,39 @@ const AttendanceUpdation = () => {
     return () => {
       reduxDispatch({ type: GET_EXCEL_DATA, payload: [], status: true })
     }
-  }, [GET_EXCEL_DATA,reduxDispatch])
+  }, [GET_EXCEL_DATA, reduxDispatch])
 
   return (
     <CustomLayout title="Attendance Marking - Manual" displayClose={true} >
       <ToastContainer />
-      <Box sx={{ display: 'flex', flex: 1, px: 0.5, flexDirection: 'column', width:'100%' }}>
-        <UpdationTopCard/>
-        <Paper square variant='outlined' elevation={0} sx={{display: "flex",alignItems: "center", }}  >
+      <Box sx={{ display: 'flex', flex: 1, px: 0.5, flexDirection: 'column', width: '100%' }}>
+        <UpdationTopCard />
+        <Paper square variant='outlined' elevation={0} sx={{ display: "flex", alignItems: "center", }}  >
           <Box sx={{ flex: 1 }} >
             <CssVarsProvider>
               <Typography startDecorator={<DragIndicatorOutlinedIcon />} textColor="neutral.400" sx={{ display: 'flex', }} >
-                Attendance Details 
+                Attendance Details
               </Typography>
             </CssVarsProvider>
-          </Box>            
+          </Box>
         </Paper>
 
         <Paper square variant="outlined" sx={{ display: 'flex', p: 0.5, flex: 1 }}>
           {/* employee Name Section */}
-          <Box sx={{width:'100%'}}>
+          <Box sx={{ width: '100%' }}>
             <TableContainer>
               <Table size="small">
                 <TableHead>
                   <TableRow>
                     <TableCell sx={{ p: 0, backgroundColor: '#F5F5F6', border: 0.1, borderColor: '#E1E6E1' }}>
-                      <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2}}>
+                      <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2 }}>
                         Name
                       </Box>
                     </TableCell>
                     <TableCell sx={{ p: 0, backgroundColor: '#F5F5F6', border: 0.1, borderColor: '#E1E6E1' }}>
-                    <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2 }}>
-                      ID #
-                    </Box>
+                      <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2 }}>
+                        ID #
+                      </Box>
                     </TableCell>
                     <TableCell sx={{ p: 0, backgroundColor: '#F5F5F6', border: 0.1, borderColor: '#E1E6E1' }}>
                       <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2 }}>
@@ -70,7 +70,7 @@ const AttendanceUpdation = () => {
                     </TableCell>
                     <TableCell sx={{ p: 0, backgroundColor: '#F5F5F6', border: 0.1, borderColor: '#E1E6E1' }}>
                       <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2 }}>
-                       Actual Worked
+                        Actual Worked
                       </Box>
                     </TableCell>
                     <TableCell sx={{ p: 0, backgroundColor: '#F5F5F6', border: 0.1, borderColor: '#E1E6E1' }}>
@@ -123,107 +123,107 @@ const AttendanceUpdation = () => {
 
                 <TableBody>
                   {excel.map((row, index) => (
-                    <TableRow key={index}sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                       <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, backgroundColor: '#f1faee', width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
-                        <Box component={Grid}item sx={{ minHeight: 25, maxHeight: 25,  p: 0.2,fontWeight: 'normal', textOverflow: 'ellipsis', width: 100,}}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.EmployeeName}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell component="th"scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1'}}>
-                        <Box component={Grid}item sx={{ minHeight: 25, maxHeight: 25, p: 0.2,fontWeight: 'normal',textOverflow: 'ellipsis', width: 100,}}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.EmployeeNo}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell component="th"scope="row"sx={{ py: 0, px: 0.5, width: 100,border: 0.1, borderColor: '#E1E6E1'}}>
-                        <Box component={Grid}item sx={{minHeight: 25,maxHeight: 25, p: 0.2, fontWeight: 'normal',textOverflow: 'ellipsis',width: 100,}}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.Total}
                           </Typography>
-                      </Box>
+                        </Box>
                       </TableCell>
-                      <TableCell component="th" scope="row"sx={{ py: 0, px: 0.5, width: 100,border: 0.1, borderColor: '#E1E6E1'}}>
-                        <Box component={Grid}item sx={{minHeight: 25, maxHeight: 25,p: 0.2, fontWeight: 'normal',textOverflow: 'ellipsis',width: 100,}}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.Worked}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell component="th"  scope="row" sx={{  py: 0, px: 0.5, width: 100,  border: 0.1, borderColor: '#E1E6E1' }}>
-                        <Box component={Grid}item sx={{ minHeight: 25,  maxHeight: 25, p: 0.2,fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.CalculatedWorked}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell component="th" scope="row" sx={{  py: 0, px: 0.5, width: 100,  border: 0.1, borderColor: '#E1E6E1'  }}>  
-                        <Box component={Grid}item  sx={{  minHeight: 25,  maxHeight: 25,  p: 0.2,  fontWeight: 'normal',  textOverflow: 'ellipsis',  width: 100,}}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.off}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell  component="th"scope="row"sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1'  }}>
-                        <Box component={Grid}item sx={{minHeight: 25,  maxHeight: 25, p: 0.2,fontWeight: 'normal', textOverflow: 'ellipsis', width: 100,}}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.Leave}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell component="th"  scope="row" sx={{  py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1'  }}>
-                        <Box component={Grid}item sx={{ minHeight: 25, maxHeight: 25,  p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.lwp}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1'}}>
-                        <Box component={Grid}item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100,}}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.lop}
                           </Typography>
                         </Box>
                       </TableCell>
-                      <TableCell component="th" scope="row" sx={{py: 0, px: 0.5, width: 100,border: 0.1, borderColor: '#E1E6E1'}}>
-                        <Box component={Grid}item sx={{ minHeight: 25,maxHeight: 25, p: 0.2, fontWeight: 'normal',textOverflow: 'ellipsis', width: 100,}}>
+                      <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
+                        <Box component={Grid} item sx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.Calculatedlop}
                           </Typography>
                         </Box>
                       </TableCell>
                       <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
-                        <Box component={Grid}itemsx={{minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal',textOverflow: 'ellipsis',width: 100,}}>
+                        <Box component={Grid} itemsx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.holiday}
                           </Typography>
                         </Box>
                       </TableCell>
                       <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
-                        <Box component={Grid}itemsx={{minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal',textOverflow: 'ellipsis',width: 100,}}>
+                        <Box component={Grid} itemsx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.holidayworked}
                           </Typography>
                         </Box>
                       </TableCell>
                       <TableCell component="th" scope="row" sx={{ py: 0, px: 0.5, width: 100, border: 0.1, borderColor: '#E1E6E1' }}>
-                        <Box component={Grid}itemsx={{minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal',textOverflow: 'ellipsis',width: 100,}}>
+                        <Box component={Grid} itemsx={{ minHeight: 25, maxHeight: 25, p: 0.2, fontWeight: 'normal', textOverflow: 'ellipsis', width: 100, }}>
                           <Typography variant="body2" gutterBottom noWrap={true}>
                             {row.total_p_day}
                           </Typography>
                         </Box>
                       </TableCell>
                     </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-    </Paper>
-  </Box>
-  </CustomLayout>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
+        </Paper>
+      </Box>
+    </CustomLayout>
   )
 }
 
