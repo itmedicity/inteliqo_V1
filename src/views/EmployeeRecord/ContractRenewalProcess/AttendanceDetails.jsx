b45a1ff867120ca15296320f844cb475af7bd220
Medicity <itmedicity@tmc.ac.in>
2024-04-18 22:49:38 +0530

    sfsfsdf

--- src/views/EmployeeRecord/ContractRenewalProcess/AttendanceDetails.jsx
+++ src/views/EmployeeRecord/ContractRenewalProcess/AttendanceDetails.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardContent, Chip, CssVarsProvider, IconButton } from '@mui/joy'
+import { Avatar, Box, Button, Card, CardActions, CardContent, Chip, CssVarsProvider, IconButton } from '@mui/joy'
 import { differenceInDays, endOfMonth, getDaysInMonth, startOfMonth } from 'date-fns';
 import moment from 'moment';
 import React, { Fragment, memo, useCallback, useEffect, useMemo, useState } from 'react'
@@ -8,6 +8,7 @@ import Typography from '@mui/joy/Typography';
 import { useDispatch } from 'react-redux';
 import { Actiontypes } from 'src/redux/constants/action.type'
 import AttributionIcon from '@mui/icons-material/Attribution';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 
 const AttendanceDetails = ({ id, no, em_cont_end }) => {
 
@@ -89,54 +90,87 @@ const AttendanceDetails = ({ id, no, em_cont_end }) => {
 
     return (
         <Fragment>
-            <Card
+            {/* <Card
                 variant="outlined"
                 color="neutral"
                 orientation="vertical"
                 size="sm"
-                sx={{ m: 0.5 }}
-            >
+            // sx={{ m: 0.5 }}
+            > */}
 
-                {/* 
-                <Card
-                    variant="outlined"
+
+            <Card
+                variant="outlined"
+                sx={{
+                    width: '100%',
+                    // overflow: 'auto',
+                    resize: 'horizontal',
+                    ml: 0.5
+                }}
+            >
+                <Box
                     sx={{
-                        width: '100%',
-                        // overflow: 'auto',
-                        resize: 'horizontal',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
                     }}
                 >
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar src="" size="lg" />
-                        <Typography level="body1">{em_no}</Typography>
-                    </Box>
-                    <CardContent>
-                        <Typography level="title-lg">{em_name}</Typography>
-                        <Typography level="body-sm">
-                            We are a community of developers prepping for coding interviews,
-                            participate, chat with others and get better at interviewing.
-                        </Typography>
-                    </CardContent>
-                    <CardActions buttonFlex="0 1 120px">
-                        <IconButton variant="outlined" color="neutral" sx={{ mr: 'auto' }}>
-                            <FavoriteBorder />
-                        </IconButton>
-                        <Button variant="outlined" color="neutral">
-                            View
-                        </Button>
-                        <Button variant="solid" color="primary">
-                            Join
-                        </Button>
-                    </CardActions>
-                </Card> */}
+                    {/* <Avatar src="" size="lg" /> */}
+                    <Typography level="title-lg">Attendance Process</Typography>
+                </Box>
+                <CardContent>
+                    <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly', alignItems: 'center' }} >
+                        <Typography level="title-md"  >Total Days :</Typography>
+                        <Typography level="title-md" color="primary" variant="plain" noWrap sx={{ mx: 0.5 }} >{No_of_days === 0 ? 0 : No_of_days}</Typography>
+                        <Typography level="title-md"  >Total Lop :</Typography>
+                        <Typography level="title-md" color="primary" variant="plain" noWrap sx={{ mx: 0.5 }} >{total_lop === 0 ? 0 : total_lop}</Typography>
 
-                {/* <Box sx={{ display: "flex", width: "100%" }} >
+                        <Typography level="title-md"  >Total Leave :</Typography>
+                        <Typography level="title-md" color="primary" variant="plain" noWrap sx={{ mx: 0.5 }} >{totalLeave === 0 ? 0 : totalLeave}</Typography>
+                        <Typography level="title-md"  >No of days Worked :</Typography>
+                        <Typography level="title-md" color="primary" variant="plain" noWrap sx={{ mx: 0.5 }} >{total_days_worked === 0 ? 0 : total_days_worked}</Typography>
+                    </Box>
+
+                </CardContent>
+                <CardActions >
+                    {/* <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+                        {
+                            attandFlag === 1 ?
+                                <Chip
+                                    color="danger"
+                                    // onClick={ProcessAttendance}
+                                    size="md"
+                                    variant="outlined"
+
+                                >Attendance Processed
+                                </Chip>
+                                : <Chip
+                                    color="success"
+                                    onClick={ProcessAttendance}
+                                    size="md"
+                                    variant="outlined"
+
+                                >Process Attendance
+                                </Chip>
+                        }
+                    </Box> */}
+
+                    {/* <IconButton variant="outlined" color="neutral" sx={{ mr: 'auto' }}>
+                        <FavoriteBorder />
+                    </IconButton> */}
+                    {
+                        attandFlag === 1 ?
+                            <Button variant="outlined" color="neutral">
+                                Attendance Processed
+                            </Button> :
+                            <Button variant="solid" fullWidth color="primary" onClick={ProcessAttendance} >
+                                Process The Attendance
+                            </Button>
+                    }
+                </CardActions>
+            </Card>
+
+            {/* <Box sx={{ display: "flex", width: "100%" }} >
                     <Chip
                         color="danger"
                         size="md"
@@ -147,7 +181,7 @@ const AttendanceDetails = ({ id, no, em_cont_end }) => {
                     </Chip>
                 </Box> */}
 
-                {/* <Box sx={{ display: "flex", width: "100%" }} >
+            {/* <Box sx={{ display: "flex", width: "100%" }} >
                     <IconButton
                         // aria-label="bookmark Bahamas Islands"
                         variant="plain"
@@ -161,7 +195,7 @@ const AttendanceDetails = ({ id, no, em_cont_end }) => {
                         <Typography level="title-lg">Attendance Details</Typography>
                     </Box>
                 </Box> */}
-                <CardContent orientation="horizontal">
+            {/* <CardContent orientation="horizontal">
 
                     <Box sx={{ display: "flex", width: "100%" }} >
                         <Box sx={{ display: "flex", flex: 1, px: 0.5, justifyContent: "space-evenly" }} >
@@ -199,8 +233,8 @@ const AttendanceDetails = ({ id, no, em_cont_end }) => {
                             </CssVarsProvider>
                         </Box>
                     </Box>
-                </CardContent>
-                <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
+                </CardContent> */}
+            {/* <Box sx={{ display: 'flex', flexDirection: 'row-reverse' }}>
                     {
                         attandFlag === 1 ?
                             <Chip
@@ -220,8 +254,8 @@ const AttendanceDetails = ({ id, no, em_cont_end }) => {
                             >Process Attendance
                             </Chip>
                     }
-                </Box>
-            </Card>
+                </Box> */}
+            {/* </Card> */}
         </Fragment>
     )
 }
