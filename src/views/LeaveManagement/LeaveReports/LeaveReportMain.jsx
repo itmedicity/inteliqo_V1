b27f91be0bbfeac58d5b3e11f6fe26a98716c2b0
unknown <Administrator@ITSYS13.medicity.co.in>
2025-03-04 10:03:39 +0530

    leave report correction

--- src/views/LeaveManagement/LeaveReports/LeaveReportMain.jsx
+++ src/views/LeaveManagement/LeaveReports/LeaveReportMain.jsx
@@ -1,33 +1,35 @@
 import React, { lazy, useEffect, useMemo, useState, } from 'react'
 import { memo } from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getCommonSettings, getEmployeeInformationLimited } from 'src/redux/reduxFun/reduxHelperFun';
+import { setCommonSetting } from 'src/redux/actions/Common.Action'
 
 const MasterComponent = lazy(() => import('./MasterPage'))
 const InchargeHod = lazy(() => import('./InchargeHodPage'))
 const EmployeeComponent = lazy(() => import('./EmployeePage'))
 
+
 const LeaveReportMain = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setCommonSetting());
+    }, [dispatch])
 
     const empInformation = useSelector((state) => getEmployeeInformationLimited(state))
     const empInformationFromRedux = useMemo(() => empInformation, [empInformation])
 
     const { hod, incharge, groupmenu } = empInformationFromRedux;
 
-    const [masterGroupStatus, setMasterGroupStatus] = useState(false);
     const getcommonSettings = useSelector((state) => getCommonSettings(state, groupmenu))
     const groupStatus = useMemo(() => getcommonSettings, [getcommonSettings])
-
-    useEffect(() => {
-        setMasterGroupStatus(groupStatus)
-    }, [groupStatus])
-
 
     return (
         <>
             {
-                (hod === 1 || incharge === 1) && masterGroupStatus === true ? <MasterComponent /> :
-                    (hod === 1 || incharge === 1) && masterGroupStatus === false ? <InchargeHod /> : <EmployeeComponent />
+                ((hod === 1 || incharge === 1) && groupStatus === true) ? <MasterComponent /> :
+                    ((hod === 1 || incharge === 1) && groupStatus === false) ? <InchargeHod /> : <EmployeeComponent />
             }
         </>
     )
